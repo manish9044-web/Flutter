81419617bdbac17abfdde1cd832018809d2b3237
manish9044-web <manishg560@gmail.com>
2024-02-10 20:40:17 +0530

    updated

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,21 +1,56 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-      child: Text("Login page", 
-      style: TextStyle(
-        fontSize: 30,
-        color: Colors.blue,
-        fontWeight: FontWeight.bold,
+    return Material(
+      color: Colors.white,
+        child: Column(
+      children: [Image.asset("assests/images/login_image.png",fit: BoxFit.fitHeight,
       ),
+      SizedBox(height: 20,
       ),
+      Text(
+        "Welcome",
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-    );
+      SizedBox(height: 20,
+      ),
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: [
+          TextFormField(
+          decoration: InputDecoration(
+            hintText: "Enter Username",
+            labelText: "username"
+          ),
+        ),
+        TextFormField(
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: "Enter password",
+            labelText: "password"
+          ),
+        ),
+        SizedBox(height: 20,
+      ),
+      ElevatedButton(
+        child: Text("login"),
+        style: TextButton.styleFrom(),
+        onPressed: () {
+          print("Hi Coder");
+        },),
+        ],),
+      )
+      ],
+    ));
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:project1/Pages/home_page.dart';
 import 'package:project1/Pages/login_page.dart';
 
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   int days = 30;
-  String name = "Flutter course"; 
+  String name = "Flutter course";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      darkTheme: ThemeData(brightness: Brightness.light),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple, 
+          ),
       routes: {
-        "/" :(context) => LoginPage(),
-        "/home":(context) => HomePage(), 
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
       },
     );
   }
